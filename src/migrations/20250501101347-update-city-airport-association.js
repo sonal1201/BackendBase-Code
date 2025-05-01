01fcfcc55b6c762fd7cee64c7c665933bc250f87
Sonal Singh <sonal1201kumar@gmail.com>
2025-05-01 16:36:29 +0530

    added forgien_key constraint for city_id with airport

---
+++ src/migrations/20250501101347-update-city-airport-association.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports', {
+      fields: ['cityId'],
+      type: 'foreign key',
+      name: 'city_fkey',
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'city_fkey');
+  }
+};
